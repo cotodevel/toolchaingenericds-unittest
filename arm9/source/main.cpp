32bc227e81fc2c93c1a41fc2444db9593f64ca43
cotodevel <programmer.coto.1988@gmail.com>
2024-03-01 01:48:18 -0300

    -void handleDSInitError(int stage, u32 fwNo) is now fused with general exceptions (blue screen) -Re-enable MPU exceptions, forgot to add them back like 2 years ago, lol. I was wondering why MPU wasn't throwing exceptions. -fix ARGV segfaults 1
    -ARM7DLDI no longer initializes from FIFO IRQ, spinlocks instead.
    -Should fix booting on Slot 1 devices on nearly any kernel
    -Note: More commits to address TGDS homebrew compatibility & issues to come
    Commit: 2/?
    
    -reduce ARM9 init spinlocks to just one, will improve bootrate across all DS systems regardless of detected firmware.
    
    Note:
    as a side effect, the firmware detection code is removed,
    because keeping a whitelist of firmwares, rendered TGDS homebrew
    useless on certain DS systems, which is unacceptable.
    Thus this commit restores TGDS homebrew running on every DS system out there.
    
    -handle FS initialization failure as a DS exception always (stage 1, ARM7DLDI fail)

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -294,7 +294,7 @@ int main(int argc, char **argv) {
 	}
 
 	bool isCustomTGDSMalloc = true;
-	setTGDSMemoryAllocator(getProjectSpecificMemoryAllocatorSetup(TGDS_ARM7_MALLOCSTART, TGDS_ARM7_MALLOCSIZE, isCustomTGDSMalloc, TGDSDLDI_ARM7_ADDRESS));
+	setTGDSMemoryAllocator(getProjectSpecificMemoryAllocatorSetup(isCustomTGDSMalloc));
 	sint32 fwlanguage = (sint32)getLanguage();
 	
 	//argv destroyed here because of xmalloc init, thus restore them
@@ -365,7 +365,7 @@ int main(int argc, char **argv) {
 			//pass incoming launcher's ARGV0
 			char arg0[256];
 			int newArgc = 3;
-			if (argc > 2) {
+			if (argc > 4) {
 				printf(" ---- test");
 				printf(" ---- test");
 				printf(" ---- test");
@@ -388,12 +388,14 @@ int main(int argc, char **argv) {
 			}
 			//debug end
 			
-			char thisArgv[4][MAX_TGDSFILENAME_LENGTH];
+			char thisArgv[5][MAX_TGDSFILENAME_LENGTH];
 			memset(thisArgv, 0, sizeof(thisArgv));
 			strcpy(&thisArgv[0][0], thisTGDSProject);	//Arg0:	This Binary loaded
 			strcpy(&thisArgv[1][0], curChosenBrowseFile);	//Arg1:	Chainload caller: TGDS-MB
-			strcpy(&thisArgv[2][0], thisTGDSProject);	//Arg2:	NDS Binary reloaded through ChainLoad
+			strcpy(&thisArgv[2][0], "0:/stub.bin");			//bugged arg slot
 			strcpy(&thisArgv[3][0], (char*)&arg0[0]);//Arg3: NDS Binary reloaded through ChainLoad's ARG0
+			strcpy(&thisArgv[4][0], thisTGDSProject);	//Arg4:	NDS Binary reloaded through ChainLoad
+			newArgc++;
 			addARGV(newArgc, (char*)&thisArgv);				
 			if(TGDSMultibootRunNDSPayload(curChosenBrowseFile) == false){ //should never reach here, nor even return true. Should fail it returns false
 				
@@ -401,7 +403,7 @@ int main(int argc, char **argv) {
 		}
 	}
 	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-	
+	setupDisabledExceptionHandler();
 	REG_IME = 0;
 	MPUSet();
 	//TGDS-Projects -> legacy NTR TSC compatibility
@@ -409,7 +411,6 @@ int main(int argc, char **argv) {
 		TWLSetTouchscreenTWLMode();
 	}
 	REG_IME = 1;
-	
 	InstallSoundSys();
 	initMIC();
 	
