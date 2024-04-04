d7954bf4e60d7dec669cee1df0af34dd37ade9b9
cotodevel <programmer.coto.1988@gmail.com>
2024-04-04 19:44:36 -0300

    -rebuild TGDS-mb from scratch (newlib-nds + TGDS + TGDS-mb), reloads OK on TWL hardware + VRAM + DSWIFI -TGDS ARM7 VRAM Core: alloc 96K for program , 16K DLDI, 16K ARM7 malloc RAM -centralize vram loader bits -96K ARM7 VRAM Core fixes -implementation arm7bootldr ARM7 VRAM module (1/3) -implementation arm7bootldr ARM7 VRAM module (2/3) -implementation arm7bootldr ARM7 VRAM module (3/3) -TGDS Project now has proper DLDI section: 32K -TGDS DLDI code is now backported 100% to ARM code -ARM7DLDI is relocated properly as it was meant to be 3 years ago. -fix TGDS-MB v3 VRAM on NTR/TWL hardware on improved DLDI code -implementation TGDS ARM7 VRAM Core (1/2) -implementation TGDS ARM7 VRAM Core (2/2) -restore DSWNIFI in TGDS ARM7 VRAM core -synchronize proper ARM7DLDI init to executeARM7Payload(u32 arm7entryaddress, int arm7BootCodeSize, u32 * payload); -allow to merge existing TGDS ARM7 IWRAM Core bits into TGDS ARM7 VRAM Core

--- arm9/source/main.cpp
+++ arm9/source/main.cpp
@@ -66,6 +66,10 @@ USA
 #include "debugNocash.h"
 #include <stdio.h>
 
+//ARM7 VRAM core
+#include "arm7vram.h"
+#include "arm7vram_twl.h"
+
 float rotateX = 0.0;
 float rotateY = 0.0;
 float camMov = -1.0;
@@ -277,6 +281,20 @@ __attribute__ ((optnone))
 int main(int argc, char **argv) {
 	
 	/*			TGDS 1.6 Standard ARM9 Init code start	*/
+	//Save Stage 1: IWRAM ARM7 payload: NTR/TWL (0x03800000)
+	memcpy((void *)TGDS_MB_V3_ARM7_STAGE1_ADDR, (const void *)0x02380000, (int)(96*1024));	//
+	coherent_user_range_by_size((uint32)TGDS_MB_V3_ARM7_STAGE1_ADDR, (int)(96*1024)); //		also for TWL binaries 
+	
+	//Execute Stage 2: VRAM ARM7 payload: NTR/TWL (0x06000000)
+	u32 * payload = NULL;
+	if(__dsimode == false){
+		payload = (u32*)&arm7vram[0];	
+	}
+	else{
+		payload = (u32*)&arm7vram_twl[0];
+	}
+	executeARM7Payload((u32)0x02380000, 96*1024, payload);
+	
 	bool isTGDSCustomConsole = false;	//set default console or custom console: default console
 	GUI_init(isTGDSCustomConsole);
 	GUI_clear();
